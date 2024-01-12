d00fff513b28d4866cfb3ee53eb0b3b4ab96b439
francis <franzdiaz460@gmail.com>
2024-01-12 20:18:58 +0400

    ref: implemented active link styling to highlight current active links that matches the url

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -1,1 +1,34 @@
 console.log("Hello World");
+
+const locationEvent = new CustomEvent("updateActiveLink", {
+  detail: location.pathname,
+});
+
+const navLinksContainer = document.querySelector("#nav-links-container > ul");
+
+function resetLinks(links) {
+  return links.map((link) => {
+    if (link.classList.contains("selected-link")) {
+      link.classList.remove("selected-link");
+      return link;
+    }
+    return link;
+  });
+}
+function setActiveLink() {
+  const navLinks = Array.from(navLinksContainer.children);
+  const links = resetLinks(navLinks);
+  links.forEach((link) => {
+    const anchor = link.children[0].href.substr(-location.pathname.length);
+    if (anchor === location.pathname) {
+      console.log(location.pathname);
+      link.setAttribute("class", "selected-link");
+      return;
+    }
+  });
+}
+
+window.addEventListener("updateActiveLink", (e) => {
+  setActiveLink();
+});
+window.dispatchEvent(locationEvent);
