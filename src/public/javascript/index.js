7c6bb543cd6021ec32afe46bdefdbf15cda4955a
francis <franzdiaz460@gmail.com>
2024-01-15 16:52:17 +0400

    feat: implemented a feature where links are targeting h1 elements for table contents

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -2,11 +2,14 @@ const locationEvent = new CustomEvent("updateActiveLink", {
   detail: location.pathname,
 });
 
-function interpolateLinkTitle(linkTitle) {
+function interpolateHashedLinkTitle(linkTitle) {
   let newTitle = "";
   for (let i = 0; i < linkTitle.length; i++) {
-    if (linkTitle[i] == "%20") {
-      newTitle += " ";
+    if (linkTitle[i] == "%" || linkTitle[i] == "2" || linkTitle[i] == "0") {
+      newTitle += "";
+      if (linkTitle[i] == "0") {
+        newTitle += " ";
+      }
     } else {
       newTitle += linkTitle[i];
     }
@@ -24,31 +27,41 @@ function resetLinks(links) {
   });
 }
 function setLinkActive(links) {
-  console.log(location.hash);
   links.forEach((link) => {
-    const anchor = link.children[0].href.substr(-location.pathname.length);
-    if (anchor === location.pathname) {
+    const anchorPath = link.children[0].href.substr(-location.pathname.length);
+    const anchorHash = link.children[0].href.substr(-location.hash.length);
+    console.log(anchorPath);
+    if (anchorPath === location.pathname) {
       console.log(location.pathname);
       link.setAttribute("class", "selected-link");
       return;
     }
     if (location.hash[0] === "#") {
-      console.log("is hashed");
-      console.log(interpolateLinkTitle(location.hash));
+      if (location.hash === `${anchorHash}`) {
+        link.setAttribute("class", "selected-link");
+        return;
+      }
     }
   });
 }
-function setActiveSideNavLink(navLinksContainer) {
+function setActiveSidebarNavLink(navLinksContainer) {
   if (!navLinksContainer) return;
   const navLinks = Array.from(navLinksContainer.children);
   const links = resetLinks(navLinks);
   setLinkActive(links);
 }
 
+function setActiveContentTableLink() {
+  const tableContentsContainer = document.querySelector(
+    "#table-contents-container > ul"
+  );
+  const tableContentLinks = Array.from(tableContentsContainer.children);
+  resetLinks(tableContentLinks);
+  setLinkActive(tableContentLinks);
+}
+
 function createContentTableLinks(tableContentsContainer) {
   const h1List = Array.from(document.querySelectorAll("h1"));
-  const tableContentsLinks = Array.from(tableContentsContainer);
-
   const newH1List = h1List.filter((header, i) => {
     if (i !== 0) {
       const content = header.textContent;
@@ -56,9 +69,12 @@ function createContentTableLinks(tableContentsContainer) {
       return header;
     }
   });
-  const newLinks = newH1List.map((header) => {
+  const newLinks = newH1List.map((header, i) => {
     const newLink = document.createElement("a");
     const liContainer = document.createElement("li");
+    if (i == 0) {
+      liContainer.setAttribute("class", "selected-link");
+    }
     newLink.href = `#${header.textContent}`;
     newLink.textContent = header.textContent;
 
@@ -78,13 +94,7 @@ window.addEventListener("updateActiveLink", (e) => {
     createContentTableLinks(tableContentsContainer);
     return;
   }
-  setActiveSideNavLink(navLinksContainer);
+  setActiveSidebarNavLink(navLinksContainer);
 });
 window.addEventListener("load", () => window.dispatchEvent(locationEvent));
-window.addEventListener("hashchange", () => {
-  const tableContentsContainer = document.querySelector(
-    "#table-contents-container > ul"
-  );
-  const tableContentLinks = Array.from(tableContentsContainer.children);
-  setLinkActive(tableContentLinks);
-});
+window.addEventListener("hashchange", () => setActiveContentTableLink());
